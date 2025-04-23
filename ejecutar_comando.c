98a670a9a63b01215198669a9485f5378d54d868
padredemellis <10656@holbertonstudents.com>
2025-04-23 19:17:07 +0000

    Shellminator ejecutar comando funcion principal

---
+++ ejecutar_comando.c
@@ -0,0 +1,48 @@
+#include "shellminator.h"
+/**
+ * ejecutar_comando - Función principal
+ * @args: Array de argumentos
+ */
+void ejecutar_comando(char **args)
+{
+	pid_t pid;
+	int status;
+	char *ruta_completa;
+
+	if (!args || !args[0])
+		return;
+
+	if (strcmp(args[0], "cd") == 0 || strcmp(args[0], "exit") == 0)
+	{
+		manejar_comando_interno(args);
+		return;
+	}
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork error");
+		return;
+	}
+	if (pid == 0)
+	{
+		ruta_completa = buscar_ruta_comando(args[0]);
+		if (ruta_completa)
+		{
+			ejecutar_comando_externo(ruta_completa, args);
+		}
+		else
+		{
+			fprintf(stderr, "%s: comando no encontrado\n", args[0]);
+			exit(127);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			printf("Estado: %d\n", WEXITSTATUS(status));
+		else if (WIFSIGNALED(status))
+			printf("Señal: %d\n", WTERMSIG(status));
+	}
+}
