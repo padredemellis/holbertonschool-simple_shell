ad17bd93aaeb966b07889d25af07ede2ab8928cd
padredemellis <10656@holbertonstudents.com>
2025-04-23 18:54:20 +0000

    Shellminator shellminator.h

---
+++ shellminator.h
@@ -0,0 +1,19 @@
+#ifndef SHELLMINATOR_H
+#define SHELLMINATOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/* Prototipos de funciones */
+void mostrar_prompt(void);
+char *leer_entrada(void);
+char **analizar_entrada(char *linea);
+void ejecutar_comando(char **args);
+void manejar_comando_interno(char **args)
+char *buscar_ruta_comando(char *comando)
+void ejecutar_comando_externo(char *ruta_completa, char **args)
+
+#endif /* SHELLMINATOR_H */
