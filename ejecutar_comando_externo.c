1e0e1f5200fd7ba8efd744522548cb5e22d8282c
padredemellis <10656@holbertonstudents.com>
2025-04-25 23:45:49 +0000

    fixed ejecutar comando externo

--- ejecutar_comando_externo.c
+++ ejecutar_comando_externo.c
@@ -6,8 +6,15 @@
  */
 void ejecutar_comando_externo(char *ruta_completa, char **args)
 {
-	execve(ruta_completa, args, environ);
-	perror("execve error");
+	if (access(ruta_completa, X_OK) == 0)
+	{
+		execve(ruta_completa, args, environ);
+		perror("execve error");
+	}
+	else
+	{
+		fprintf(stderr, "%s: comando no encontrado\n", args[0]);
+	}
 	free(ruta_completa);
 	exit(EXIT_FAILURE);
 }
