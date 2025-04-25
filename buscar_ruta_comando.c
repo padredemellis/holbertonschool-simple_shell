4c539e0b0822c314dc6185c5939d7b026e8c0ef0
padredemellis <10656@holbertonstudents.com>
2025-04-25 23:55:48 +0000

    fixed buscar ruta comando

--- buscar_ruta_comando.c
+++ buscar_ruta_comando.c
@@ -1,38 +1,43 @@
 #include "shellminator.h"
+#include <string.h>
+
 /**
- * buscar_ruta_comando - Busca el comando en el PATH
+ * buscar_ruta_comando - Busca el comando en el PATH usando environ
  * @comando: Nombre del comando
  * Return: Ruta completa o NULL
  */
 char *buscar_ruta_comando(char *comando)
 {
-	char *path_env = getenv("PATH");
+	char *path_env = NULL;
 	char *path_copy, *path_token, *ruta_completa = NULL;
+	int i;
 
-	if (strchr(comando, '/') || !path_env)
-	{
+	for (i = 0; environ[i] != NULL; i++) {
+		if (strncmp(environ[i], "PATH=", 5) == 0) {
+			path_env = environ[i] + 5;
+			break;
+		}
+	}
+
+	if (strchr(comando, '/') || path_env == NULL) {
 		return (strdup(comando));
 	}
-	path_copy = strdup(path_env);
 
+	path_copy = strdup(path_env);
 	if (!path_copy)
 		return (NULL);
 
 	path_token = strtok(path_copy, ":");
-
-	while (path_token)
-	{
+	while (path_token) {
 		int tam_ruta = strlen(path_token) + strlen(comando) + 2;
-
 		ruta_completa = malloc(tam_ruta);
 
-		if (!ruta_completa)
-		{
+		if (!ruta_completa) {
 			free(path_copy);
 			return (NULL);
 		}
-		sprintf(ruta_completa, "%s/%s", path_token, comando);
 
+		sprintf(ruta_completa, "%s/%s", path_token, comando);
 		if (access(ruta_completa, X_OK) == 0)
 			break;
 
@@ -40,6 +45,7 @@ char *buscar_ruta_comando(char *comando)
 		ruta_completa = NULL;
 		path_token = strtok(NULL, ":");
 	}
+
 	free(path_copy);
 	return (ruta_completa);
 }
